3656d0c83cb6d942328d3d317935ccfe70632ba8
Kateryna Shynkarenko <kshynkarenko@intellias.com>
2019-08-05 15:33:05 +0300

    Lecture3(4)

---
+++ Lecture3/Lecture3(4)/Lecture3(4)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3_4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Display the pattern like right angle triangle which repeat a number in a row:\n");
+
+            Console.Write("Input number of rows : ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{i}");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+       
